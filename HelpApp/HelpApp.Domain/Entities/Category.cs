17ed0d316695c3209b214f3fe1bbee238e9ad00c
VictorHugoTesti <victortesti06@gmail.com>
2025-02-24 12:34:11 -0300

    Criação Básica de Dominios de Entidades

---
+++ HelpApp/HelpApp.Domain/Entities/Category.cs
@@ -0,0 +1,35 @@
+﻿using HelpApp.Domain.Validation;
+using System.Collections.Generic;
+
+namespace HelpApp.Domain.Entities
+{
+    public class Category
+    {
+        #region Atributes
+        public int Id { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region Constructors
+        public Category(string name) 
+        {
+            Name= name;
+        }
+
+        public ICollection<Product> Products { get; set; }
+        #endregion
+
+        #region Validations
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name, name is Requerid");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too start, minimun 3 characters");
+
+            Name= name;
+        }
+        #endregion
+    }
+}
---
+++ HelpApp/HelpApp.Domain/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using HelpApp.Domain.Entities;
+
+namespace HelpApp.Domain.Entities
+{
+    public class Product
+    {
+        #region Atributes
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; } 
+        public int Stack { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public Product(string name, string description, decimal price, int stack, string image)
+        {
+            Name= name;
+            Description= description;
+            Price= price;
+            Stack= stack;
+            Image= image;
+        }
+
+        public Category Category { get; set; }
+        #endregion
+    }
+}
