ef8b22b8707a32027330444d3200c0b065624df3
VictorHugoTesti <victortesti06@gmail.com>
2025-04-14 12:49:41 -0300

    Aula 1404 - Conectores Base

---
+++ HelpApp/HelpApp.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HelpApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HelpApp.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletronicos"),
+                new Category(3, "Acessorios")
+            );
+        }
+    }
+}
