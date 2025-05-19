577e4bafe2e6942271cb5490182fe4a01de58379
VictorHugoTesti <victortesti06@gmail.com>
2025-05-19 12:41:20 -0300

    Modelo de MediatR e Command de Produtos

---
+++ HelpApp/HelpApp.Application/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using HelpApp.Application.DTOs;
+
+namespace HelpApp.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetById(int? id);
+        Task Add(ProductDTO productDTO);
+        Task Update(ProductDTO productDTO);
+        Task Remove(int? id);
+    }
+}
